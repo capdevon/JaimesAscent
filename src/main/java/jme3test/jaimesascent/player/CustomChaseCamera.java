b08aa5ea59a1577a69a4e66e987f07ae375b42c2
capdevon <jefferydeaver2010@gmail.com>
2025-01-19 18:45:29 +0100

    CustomChaseCamera: javadoc

--- src/main/java/jme3test/jaimesascent/player/CustomChaseCamera.java
+++ src/main/java/jme3test/jaimesascent/player/CustomChaseCamera.java
@@ -6,11 +6,20 @@ import com.jme3.renderer.Camera;
 import com.jme3.scene.Spatial;
 
 /**
+ * A camera that follows a spatial object and can rotate around it by dragging
+ * the mouse.
  *
  * @author capdevon
  */
 public class CustomChaseCamera extends ChaseCamera {
 
+    /**
+     * Constructs a CustomChaseCamera and registers input controls.
+     *
+     * @param cam the application camera
+     * @param target the spatial object to follow
+     * @param inputManager the input manager of the application to register inputs
+     */
     public CustomChaseCamera(Camera cam, Spatial target, InputManager inputManager) {
         super(cam, target, inputManager);
     }
